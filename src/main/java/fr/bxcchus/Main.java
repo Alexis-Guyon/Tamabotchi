64ad265d762a43d1e0c439f7304e1b8174c04a3f
Alexis Guyon <alexis.guyon@epitech.eu>
2022-10-28 21:27:00 +0200

    [*] Main.java

--- src/main/java/fr/bxcchus/Main.java
+++ src/main/java/fr/bxcchus/Main.java
@@ -1,11 +1,22 @@
 package fr.bxcchus;
 
+import fr.bxcchus.commands.PingCommand;
 import fr.bxcchus.database.Database;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 
 public class Main {
     public static void main(String[] args) {
-        Database db = Database.getInstance();
+        Database database = Database.getInstance();
+        JDABuilder.createLight(DiscordToken.TOKEN,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.DIRECT_MESSAGES,
+                        GatewayIntent.MESSAGE_CONTENT)
+                .setActivity(Activity.competing("Tamabotchi WIP..."))
+                .addEventListeners(new PingCommand())
+                .build();
 
     }
 }
