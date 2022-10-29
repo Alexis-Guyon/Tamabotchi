d605574f1e6a62579184e9e6dc4f2a6c9a13929a
Alexis Guyon <alexis.guyon@epitech.eu>
2022-10-30 00:34:05 +0200

    [*] Main.java

--- src/main/java/fr/bxcchus/Main.java
+++ src/main/java/fr/bxcchus/Main.java
@@ -5,9 +5,15 @@ import fr.bxcchus.commands.DeleteCommand;
 import fr.bxcchus.commands.PingCommand;
 import fr.bxcchus.commands.ShowCommand;
 import fr.bxcchus.database.Database;
+import fr.bxcchus.events.TamabotchiHappinessEvent;
+import fr.bxcchus.events.TamabotchiHungerEvent;
+import fr.bxcchus.events.TamabotchiHydrationEvent;
+import fr.bxcchus.events.TamabotchiPoopnessEvent;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.util.Timer;
 
 
 public class Main {
@@ -23,6 +29,11 @@ public class Main {
                 .addEventListeners(new ShowCommand())
                 .addEventListeners(new DeleteCommand())
                 .build();
+        Timer timer = new Timer();
+        timer.schedule(new TamabotchiPoopnessEvent(), 1000, 50000);
+        timer.schedule(new TamabotchiHungerEvent(), 1000, 10000);
+        timer.schedule(new TamabotchiHydrationEvent(), 1000, 10000);
+        timer.schedule(new TamabotchiHappinessEvent(), 1000, 30000);
 
     }
 }
