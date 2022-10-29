e30f10f423e64e47ec382d188f344822cc72d6a0
Alexis Guyon <alexis.guyon@epitech.eu>
2022-10-30 00:53:07 +0200

    [+] TamabotchiHpEvent.java

---
+++ src/main/java/fr/bxcchus/events/TamabotchiHpEvent.java
@@ -0,0 +1,29 @@
+package fr.bxcchus.events;
+
+import fr.bxcchus.entities.Tamabotchi;
+import fr.bxcchus.managers.PlayerManager;
+import fr.bxcchus.managers.TamabotchieManager;
+
+import java.sql.SQLException;
+import java.util.TimerTask;
+
+public class TamabotchiHpEvent extends TimerTask {
+    @Override
+    public void run() {
+        try {
+            for (Tamabotchi tamabotchi : TamabotchieManager.getInstance().getTamabotchis()) {
+                TamabotchieManager.getInstance().updateTamabotchiHappiness(tamabotchi.getId(), tamabotchi.getName(), tamabotchi.getLvl(), tamabotchi.getHp(), tamabotchi.getHappiness(), tamabotchi.getHydration(), tamabotchi.getHunger(), tamabotchi.getRace().getId(), tamabotchi.getRace().getName(), tamabotchi.getPlayer().getUid(), tamabotchi.getPlayer().getUsername(), tamabotchi.getDeath().getId(), tamabotchi.getDeath().getCause(), tamabotchi.getPoopness());
+
+                if (tamabotchi.getHp() <= 0) {
+                    System.out.println(tamabotchi.getName() + " is dead because he has no hp. The owner was: " + tamabotchi.getPlayer().getUsername());
+
+                    TamabotchieManager.getInstance().deleteTamabotchi(tamabotchi.getId(), tamabotchi.getName(), tamabotchi.getLvl(), tamabotchi.getHp(), tamabotchi.getHappiness(), tamabotchi.getHydration(), tamabotchi.getHunger(), tamabotchi.getRace().getId(), tamabotchi.getRace().getName(), tamabotchi.getPlayer().getUid(), tamabotchi.getPlayer().getUsername(), tamabotchi.getDeath().getId(), tamabotchi.getDeath().getCause(), tamabotchi.getPoopness());
+                    PlayerManager.getInstance().removePlayer(tamabotchi.getPlayer().getUid(), tamabotchi.getPlayer().getUsername());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
