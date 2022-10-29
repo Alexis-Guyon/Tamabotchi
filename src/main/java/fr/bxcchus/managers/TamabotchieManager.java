a85f7b969626d1c42a253e5232bdf4968973c112
Alexis Guyon <alexis.guyon@epitech.eu>
2022-10-30 00:33:41 +0200

    [*] TamabotchieManager.java

--- src/main/java/fr/bxcchus/managers/TamabotchieManager.java
+++ src/main/java/fr/bxcchus/managers/TamabotchieManager.java
@@ -1,5 +1,6 @@
 package fr.bxcchus.managers;
 
+import fr.bxcchus.entities.Death;
 import fr.bxcchus.entities.Player;
 import fr.bxcchus.entities.Race;
 import fr.bxcchus.entities.Tamabotchi;
@@ -17,21 +18,57 @@ public class TamabotchieManager {
     }
     private TamabotchieManager() {}
 
-    public boolean createTamabotchie(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username) throws SQLException {
+    public boolean createTamabotchie(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username, int deathId, String cause, int poopness) throws SQLException {
         Race race = new Race(raceId, raceName);
         Player player = new Player(uid, username);
-        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player);
+        Death death = new Death(deathId, cause);
+        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player, death, poopness);
         TamabotchiRepository.getInstance().addTamabotchi(tamabotchi);
         return true;
 
     }
-    public boolean deleteTamabotchi(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username) throws SQLException {
+    public boolean deleteTamabotchi(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username, int deathId, String cause, int poopness) throws SQLException {
         Race race = new Race(raceId, raceName);
         Player player = new Player(uid, username);
-        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player);
+        Death death = new Death(deathId, cause);
+        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player, death, poopness);
         TamabotchiRepository.getInstance().removeTamabotchi(tamabotchi);
         return true;
     }
+    public boolean updateTamabotchiPoop(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username, int deathId, String cause, int poopness) throws SQLException {
+        Race race = new Race(raceId, raceName);
+        Player player = new Player(uid, username);
+        Death death = new Death(deathId, cause);
+        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player, death, poopness);
+        TamabotchiRepository.getInstance().updateTamabotchiPoop(tamabotchi);
+        return true;
+    }
+    public boolean updateTamabotchiHydration(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username, int deathId, String cause, int poopness) throws SQLException {
+        Race race = new Race(raceId, raceName);
+        Player player = new Player(uid, username);
+        Death death = new Death(deathId, cause);
+        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player, death, poopness);
+        TamabotchiRepository.getInstance().updateTamabotchiHydration(tamabotchi);
+        return true;
+    }
+    public boolean updateTamabotchiHappiness(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username, int deathId, String cause, int poopness) throws SQLException {
+        Race race = new Race(raceId, raceName);
+        Player player = new Player(uid, username);
+        Death death = new Death(deathId, cause);
+        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player, death, poopness);
+        TamabotchiRepository.getInstance().updateTamabotchiHappiness(tamabotchi);
+        return true;
+    }
+
+    public boolean updateTamabotchiHunger(int id, String name, int level, int hp, int happiness, int hydration, int hunger, int raceId, String raceName, String uid, String username, int deathId, String cause, int poopness) throws SQLException {
+        Race race = new Race(raceId, raceName);
+        Player player = new Player(uid, username);
+        Death death = new Death(deathId, cause);
+        Tamabotchi tamabotchi = new Tamabotchi(id, name, level, hp, happiness, hydration, hunger, race, player, death, poopness);
+        TamabotchiRepository.getInstance().updateTamabotchiHunger(tamabotchi);
+        return true;
+    }
+
 
     public List<Tamabotchi> getTamabotchis() throws SQLException {
         return TamabotchiRepository.getInstance().getTamabotchis();
